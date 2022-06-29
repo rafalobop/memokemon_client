2df174c3a29f0a1cffcf0e25e3077ceb43bd7340
rafa <r.loboplaza@hotmail.com>
2022-06-29 08:47:53 -0300

    fixing route

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 
-export const backendUrl = 'http://localhost:3000/api';
-//export const backendUrl = 'https://memokemon.herokuapp.com'
+//export const backendUrl = 'http://localhost:3000/api';
+export const backendUrl = 'https://memokemon.herokuapp.com/api'
